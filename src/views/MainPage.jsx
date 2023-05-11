cec90f8bc752e6ea70006a983e25038e2a1a9a74
iosipov <iosipov@emphasoft.com>
2023-05-11 22:32:37 +0300

    added main page + form integration

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -1,18 +1,23 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React from 'react';
 import MainLayout from "../layouts/main";
-import {useDispatch, useSelector} from "react-redux";
-import {socket} from "../socket";
-import {fetchRooms} from "../redux/slices/roomsSlice";
+import {Paper, Typography} from "@mui/material";
+import './MainView.scss'
 
 const MainPage = () => {
-    const {_id: id } = useSelector(state => state.user.data)
-    const [localStream, setLocalStream] = useState();
-    const localVideo = useRef();
-    const dispatch = useDispatch()
     return (
         <MainLayout>
-            <div>
-            </div>
+            <Paper square className="main-view">
+                <Typography variant='h3' component='div'>
+                    Start your own conference by clicking "add new room" button in a sidebar!
+                </Typography>
+                <Typography variant='h3' component='div'>
+                    Feel free to share our opinion !
+                </Typography>
+                <iframe
+                    src="https://docs.google.com/forms/d/e/1FAIpQLSeQXlXKCrc2nNHViNrvejiNEhebLem57iJXwi0mqZrK34t3fA/viewform?embedded=true"
+                    width="740" height="400" frameBorder="0" marginHeight="0" marginWidth="0">Загрузка…
+                </iframe>
+            </Paper>
         </MainLayout>
     );
 };
